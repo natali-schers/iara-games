6d49c5107fad1c3ffe220b94aa8584045e7f9d00
Natali Schers <schersnatali@gmail.com>
2025-10-30 23:08:06 -0300

    feat: implement and style not found page

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,7 +1,26 @@
+import notFoundImage from '../assets/images/not-found.png';
+import { Flex } from 'reflexbox';
+import styled from 'styled-components';
+import { Spaces } from '../shared/DesignTokens';
+
+const Image = styled.img`
+    max-width: 460px;
+    width: 100%;
+    margin: ${Spaces.FOUR} 0 ${Spaces.ONE_HALF};
+    height: auto;
+`;
+
 export function NotFound() {
     return (
-        <div>
-            Página Não Encontrada!
-        </div>
+        <Flex flexDirection="column" alignItems="center">
+            <h1>Ops... Página não encontrada!</h1>
+            <p>A página que você está procurando não existe ou não pode ser acessada no momento.</p>
+            <Image src={notFoundImage} alt="Página não encontrada" />
+
+            {/* ToDo: descomentar botão após implementação da página inicial! */}
+            {/* <Flex>
+                <Button variant="primary" type="button" onClick={() => navigate('/')}>Ir para página inicial</Button>      
+            </Flex> */}
+        </Flex>
     );
 }
